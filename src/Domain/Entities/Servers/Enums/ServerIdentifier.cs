b2ea5aa59b1ca1043cb2b00204a93639d846106a
Fateehs <benzer1010@gmail.com>
2023-09-27 19:59:48 +0300

    webapi: add game server configuration

---
+++ src/Domain/Entities/Servers/Enums/ServerIdentifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crpg.Domain.Entities.Servers.Enums;
+public enum ServerIdentifier
+{
+    Crpg01,
+    Crpg02,
+    Crpg03,
+    // ... other server identifiers ...
+}
---
+++ src/Domain/Entities/Servers/GameServerConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Crpg.Domain.Common;
+using Crpg.Domain.Entities.Servers.Enums;
+
+namespace Crpg.Domain.Entities.Servers;
+public class GameServerConfig : AuditableEntity
+{
+    public ServerIdentifier Identifier { get; set; }
+    public string? ServerName { get; set; }
+    public string? GameType { get; set; }
+    public string? GamePassword { get; set; }
+    public string? WelcomeMessage { get; set; }
+    public List<string> Maps { get; set; } = new List<string>();
+    public bool? AllowPollsToKickPlayers { get; set; }
+    public bool? AllowPollsToChangeMaps { get; set; }
+    public string? DisableCultureVoting { get; set; }
+}
