3f3076b19d899aff39fe8580074a0fc98381b318
Fateehs <benzer1010@gmail.com>
2023-09-29 19:37:02 +0300

    webapi: game server configuration added

---
+++ src/Domain/Entities/GameServers/GameType.cs
@@ -0,0 +1,12 @@
+﻿namespace Crpg.Domain.Entities.Servers;
+[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element should begin with upper-case letter", Justification = "<Pending>")]
+[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1404:Code analysis suppression should have justification", Justification = "<Pending>")]
+public enum GameType
+{
+    cRPGBattle,
+    cRPGConquest,
+    cRPGDTV,
+    cRPGDuel,
+    cRPGSiege,
+    cRPGTeamDeathmatch,
+}
---
+++ src/Domain/Entities/GameServers/ServerIdentifier.cs
@@ -0,0 +1,30 @@
+﻿namespace Crpg.Domain.Entities.Servers;
+[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element should begin with upper-case letter", Justification = "<Pending>")]
+[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1404:Code analysis suppression should have justification", Justification = "<Pending>")]
+public enum ServerIdentifier
+{
+    // <summary>
+    // 01=Eu 02=NA 03=AS 04=OCE
+    // A=Battle B=Siege C=Duel D=Skirmish E=DTV
+    // </summary>
+    crpg01a,
+    crpg01b,
+    crpg01c,
+    crpg01d,
+    crpg01e,
+    crpg02a,
+    crpg02b,
+    crpg02c,
+    crpg02d,
+    crpg02e,
+    crpg03a,
+    crpg03b,
+    crpg03c,
+    crpg03d,
+    crpg03e,
+    crpg04a,
+    crpg04b,
+    crpg04c,
+    crpg04d,
+    crpg04e,
+}
