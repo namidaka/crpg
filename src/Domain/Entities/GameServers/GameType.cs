34fc91025f6cf372386d94f96fdde77bfa8a9c67
Fateehs <benzer1010@gmail.com>
2023-09-29 23:02:40 +0300

    upper-lower case attribute removed

--- src/Domain/Entities/GameServers/GameType.cs
+++ src/Domain/Entities/GameServers/GameType.cs
@@ -1,6 +1,4 @@
 ﻿namespace Crpg.Domain.Entities.Servers;
-[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element should begin with upper-case letter", Justification = "<Pending>")]
-[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1404:Code analysis suppression should have justification", Justification = "<Pending>")]
 public enum GameType
 {
     cRPGBattle,
