13879098f2b75319dc80882b7e99dbbe2bf9d947
Fateehs <benzer1010@gmail.com>
2023-10-02 12:05:08 +0300

    buidler configuration added

---
+++ src/Persistence/Configurations/GameServerConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Crpg.Domain.Entities.Servers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Crpg.Persistence.Configurations;
+public class GameServerConfiguration : IEntityTypeConfiguration<GameServerConfig>
+{
+    public void Configure(EntityTypeBuilder<GameServerConfig> builder)
+    {
+        builder.HasKey(g => g.Id);
+    }
+}
