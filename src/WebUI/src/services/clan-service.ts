010d7dc7f6c74e1db36ca202ba046580cf0eed29
Valeriy Sinevich <zohan33333@gmail.com>
2023-11-16 21:03:06 +0000

    improve item card

--- src/WebUI/src/services/clan-service.ts
+++ src/WebUI/src/services/clan-service.ts
@@ -10,6 +10,7 @@ import {
   type ClanArmoryItem,
 } from '@/models/clan';
 import { Region } from '@/models/region';
+import { type UserItem } from '@/models/user';
 import { get, post, put, del } from '@/services/crpg-client';
 import { rgbHexColorToArgbInt, argbIntToRgbHexColor } from '@/utils/color';
 
@@ -121,166 +122,7 @@ export const canKickMemberValidate = (
   );
 };
 
-//
-export const getClanArmory = (clanId: number) => {
-  // TODO: GET
-  return get<ClanArmoryItem[]>(`/clans/${clanId}/armory`);
-
-  // const mock = [
-  //   {
-  //     item: {
-  //       id: 'crpg_lion_imprinted_saber_h1',
-  //       baseId: 'crpg_lion_imprinted_saber_h1',
-  //       createdAt: '2023-07-13T21:43:44.0741909Z',
-  //       rank: 1,
-  //       name: 'Wooden Sword',
-  //       culture: 'Neutral',
-  //       type: 'OneHandedWeapon',
-  //       price: 257,
-  //       tier: 1.0245335,
-  //       requirement: 0,
-  //       weight: 1.42,
-  //       flags: [],
-  //       armor: null,
-  //       mount: null,
-  //       weapons: [
-  //         {
-  //           class: 'OneHandedSword',
-  //           itemUsage: 'onehanded_block_shield_swing_thrust',
-  //           accuracy: 0,
-  //           missileSpeed: 0,
-  //           stackAmount: 0,
-  //           length: 95,
-  //           balance: 0.75,
-  //           handling: 95,
-  //           bodyArmor: 1,
-  //           flags: ['MeleeWeapon', 'NoBlood'],
-  //           thrustDamage: 10,
-  //           thrustDamageType: 'Blunt',
-  //           thrustSpeed: 92,
-  //           swingDamage: 10,
-  //           swingDamageType: 'Blunt',
-  //           swingSpeed: 92,
-  //         },
-  //       ],
-  //     } as ClanArmoryItem['item'],
-  //     owner: {
-  //       id: 1,
-  //       platform: 'Steam',
-  //       platformUserId: '76561197987525637',
-  //       name: 'takeo',
-  //       avatar:
-  //         'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/2c/2ce4694f06523a2ffad501f5dc30ec7a8008e90e_full.jpg',
-  //       region: 'Eu',
-  //       clan: {
-  //         id: 1,
-  //         tag: 'PEC',
-  //         primaryColor: '4278190318',
-  //         secondaryColor: '4294957414',
-  //         name: 'Pecores',
-  //         bannerKey: '',
-  //         region: 'Eu',
-  //       },
-  //     },
-  //     borrower: null,
-  //   },
-  //   {
-  //     item: {
-  //       id: 'crpg_khuzait_lance_3_t5',
-  //       baseId: 'crpg_khuzait_lance_3_t5',
-  //       createdAt: '2023-07-13T21:43:44.0741909Z',
-  //       rank: 0,
-  //       name: 'Noble Cavalry Lance',
-  //       culture: 'Khuzait',
-  //       type: 'Polearm',
-  //       price: 2593,
-  //       tier: 3.7166684,
-  //       requirement: 0,
-  //       weight: 2.03,
-  //       flags: ['NotStackable'],
-  //       armor: null,
-  //       mount: null,
-  //       weapons: [
-  //         {
-  //           class: 'OneHandedPolearm',
-  //           itemUsage: 'onehanded_polearm_block_long_rshield_thrust',
-  //           accuracy: 0,
-  //           missileSpeed: 0,
-  //           stackAmount: 0,
-  //           length: 200,
-  //           balance: 0,
-  //           handling: 62,
-  //           bodyArmor: 0,
-  //           flags: ['MeleeWeapon', 'WideGrip'],
-  //           thrustDamage: 25,
-  //           thrustDamageType: 'Pierce',
-  //           thrustSpeed: 82,
-  //           swingDamage: 0,
-  //           swingDamageType: 'Undefined',
-  //           swingSpeed: 18,
-  //         },
-  //         {
-  //           class: 'TwoHandedPolearm',
-  //           itemUsage: 'polearm_block_long_shield_thrust',
-  //           accuracy: 0,
-  //           missileSpeed: 0,
-  //           stackAmount: 0,
-  //           length: 200,
-  //           balance: 0,
-  //           handling: 58,
-  //           bodyArmor: 0,
-  //           flags: ['MeleeWeapon', 'NotUsableWithOneHand', 'WideGrip', 'TwoHandIdleOnMount'],
-  //           thrustDamage: 25,
-  //           thrustDamageType: 'Pierce',
-  //           thrustSpeed: 91,
-  //           swingDamage: 0,
-  //           swingDamageType: 'Undefined',
-  //           swingSpeed: 68,
-  //         },
-  //         {
-  //           class: 'TwoHandedPolearm',
-  //           itemUsage: 'polearm_couch',
-  //           accuracy: 0,
-  //           missileSpeed: 0,
-  //           stackAmount: 0,
-  //           length: 200,
-  //           balance: 0,
-  //           handling: 62,
-  //           bodyArmor: 0,
-  //           flags: ['MeleeWeapon', 'WideGrip'],
-  //           thrustDamage: 25,
-  //           thrustDamageType: 'Pierce',
-  //           thrustSpeed: 82,
-  //           swingDamage: 0,
-  //           swingDamageType: 'Undefined',
-  //           swingSpeed: 18,
-  //         },
-  //       ],
-  //     },
-  //     owner: {
-  //       id: 2,
-  //       platform: 'Steam',
-  //       platformUserId: '76561197987525637',
-  //       name: 'orle',
-  //       avatar: 'https://avatars.steamstatic.com/d51d5155b1a564421c0b3fd5fb7eed7c4474e73d_full.jpg',
-  //       region: 'Eu',
-  //       clan: {
-  //         id: 1,
-  //         tag: 'PEC',
-  //         primaryColor: '4278190318',
-  //         secondaryColor: '4294957414',
-  //         name: 'Pecores',
-  //         bannerKey: '',
-  //         region: 'Eu',
-  //       },
-  //     },
-  //     borrower: null,
-  //   },
-  // ] as ClanArmoryItem[];
-
-  // return Promise.resolve([...mock]);
-  // return Promise.resolve([]);
-};
+export const getClanArmory = (clanId: number) => get<ClanArmoryItem[]>(`/clans/${clanId}/armory`);
 
 export const addItemToClanArmory = (clanId: number, userItemId: number) =>
   post(`/clans/${clanId}/armory`, {
@@ -295,3 +137,21 @@ export const borrowItemFromClanArmory = (clanId: number, userItemId: number) =>
 
 export const returnItemToClanArmory = (clanId: number, userItemId: number) =>
   put(`/clans/${clanId}/armory/${userItemId}/return`);
+
+export const getClanArmoryItemBorrower = (
+  clanArmoryItem: ClanArmoryItem,
+  clanMembers: ClanMember[]
+) => {
+  if (clanArmoryItem.borrow === null) return null;
+  return clanMembers.find(cm => cm.user.id === clanArmoryItem.borrow!.userId)?.user || null;
+};
+
+export const getClanArmoryItemOwner = (
+  userItem: UserItem,
+  userId: number,
+  clanMembers: ClanMember[]
+) => {
+  if (!userItem.isArmoryItem) return null;
+  if (userItem.userId === userId) return null;
+  return clanMembers.find(cm => cm.user.id === userItem.userId)?.user || null;
+};
