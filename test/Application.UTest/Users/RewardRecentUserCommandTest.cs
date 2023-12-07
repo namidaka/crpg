84f3444db4ac2b045272181d5fc3d0a1b2209d33
Valeriy Sinevich <zohan33333@gmail.com>
2023-12-07 04:50:03 +0000

    reward recent users command. need spec

---
+++ test/Application.UTest/Users/RewardRecentUserCommandTest.cs
@@ -0,0 +1,44 @@
+﻿using Crpg.Application.Characters.Commands;
+using Crpg.Application.Common;
+using Crpg.Application.Common.Results;
+using Crpg.Application.Common.Services;
+using Crpg.Application.Users.Commands;
+using Crpg.Domain.Entities.Users;
+using Moq;
+using NUnit.Framework;
+
+namespace Crpg.Application.UTest.Users;
+
+public class RewardRecentUserCommandTest : TestBase
+{
+    private static readonly Constants Constants = new()
+    {
+    };
+
+    [Test]
+    public async Task Basic()
+    {
+        User user = new()
+        {
+            Gold = 200,
+            HeirloomPoints = 1,
+        };
+        ArrangeDb.Users.Add(user);
+        await ArrangeDb.SaveChangesAsync();
+
+        Mock<IExperienceTable> experienceTableMock = new();
+        Mock<ICharacterService> characterServiceMock = new();
+
+        RewardRecentUserCommand.Handler handler = new(ActDb, Constants, characterServiceMock.Object,
+        experienceTableMock.Object);
+        await handler.Handle(new RewardRecentUserCommand { }, CancellationToken.None);
+
+        // character = await AssertDb.Characters
+        //     .Include(c => c.User)
+        //     .FirstAsync(c => c.Id == character.Id);
+
+        // Assert.That(res.Errors, Is.Null);
+        // Assert.That(res.Data!.Gold, Is.EqualTo(300));
+        // Assert.That(res.Data.HeirloomPoints, Is.EqualTo(3));
+    }
+}
