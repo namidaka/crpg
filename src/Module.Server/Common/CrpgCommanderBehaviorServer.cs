e65a7560b18022852afba0d183aff9c5f119c543
Muparadzi <harrygardner123@gmail.com>
2023-12-22 17:31:40 +0000

    init behavior

---
+++ src/Module.Server/Common/CrpgCommanderBehaviorServer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace Crpg.Module.Common;
+internal class CrpgCommanderBehaviorServer : MissionBehavior
+{
+    public override MissionBehaviorType BehaviorType => MissionBehaviorType.Other;
+
+    private class Command
+    {
+        private Agent? _commander;
+        public BattleSideEnum Side { get; }
+        public Agent? Commander
+        {
+            get => _commander ?? null;
+            set
+            {
+                if (value != _commander)
+                {
+                    _commander = value;
+                }
+            }
+        }
+
+        public Command(BattleSideEnum side)
+        {
+            Side = side;
+        }
+
+    }
+}
