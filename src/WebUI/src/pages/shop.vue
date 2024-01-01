4f0c932e61236cb29a8e2ac7422798e3c7a4b016
Valeriy Sinevich <zohan33333@gmail.com>
2024-01-01 17:32:48 +0000

    meow

--- src/WebUI/src/pages/shop.vue
+++ src/WebUI/src/pages/shop.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { create, insert, remove, search, searchVector, insertMultiple } from '@orama/orama';
+import { createItemIndex } from '@/services/item-search-service/indexator';
+import { documentsStore as defaultDocumentsStore } from '@orama/orama/components';
 import { WeaponUsage, type ItemFlat } from '@/models/item';
 import {
   getItems,
@@ -35,310 +38,131 @@ const { state: items, execute: loadItems } = useAsyncState(() => getItems(), [],
 
 await Promise.all([loadItems(), userStore.fetchUserItems()]);
 
-const {
-  itemTypeModel,
-  weaponClassModel,
-  filterModel,
-  updateFilter,
-  hideOwnedItemsModel,
-  filteredByClassFlatItems,
-  aggregationsConfig,
-  aggregationsConfigVisible,
-  aggregationByType,
-  aggregationByClass,
-  scopeAggregations,
-} = useItemsFilter(items.value);
-const { searchModel } = useSearchDebounced();
+// const {
+//   itemTypeModel,
+//   weaponClassModel,
+//   filterModel,
+//   updateFilter,
+//   hideOwnedItemsModel,
+//   filteredByClassFlatItems,
+//   aggregationsConfig,
+//   aggregationsConfigVisible,
+//   aggregationByType,
+//   aggregationByClass,
+//   scopeAggregations,
+// } = useItemsFilter(items.value);
+// const { searchModel } = useSearchDebounced();
 
-const { pageModel, perPageModel, perPageConfig } = usePagination();
-const { sortingModel, sortingConfig, getSortingConfigByField } = useItemsSort(aggregationsConfig);
+// const { pageModel, perPageModel, perPageConfig } = usePagination();
+// const { sortingModel, sortingConfig, getSortingConfigByField } = useItemsSort(aggregationsConfig);
 
-const {
-  isCompare,
-  toggleCompare,
-  compareList,
-  toggleToCompareList,
-  addAllToCompareList,
-  removeAllFromCompareList,
-} = useItemsCompare();
+// const {
+//   isCompare,
+//   toggleCompare,
+//   compareList,
+//   toggleToCompareList,
+//   addAllToCompareList,
+//   removeAllFromCompareList,
+// } = useItemsCompare();
 
-const searchResult = computed(() =>
-  getSearchResult({
-    items: filteredByClassFlatItems.value,
-    userItemsIds: hideOwnedItemsModel.value ? userItemsIds.value : [],
-    aggregationConfig: aggregationsConfig.value,
-    sortingConfig: sortingConfig.value,
-    sort: sortingModel.value,
-    page: pageModel.value,
-    perPage: perPageModel.value,
-    query: searchModel.value,
-    filter: {
-      ...filterModel.value,
-      ...(isCompare.value && { modId: compareList.value }),
+// const searchResult = computed(() =>
+//   getSearchResult({
+//     items: filteredByClassFlatItems.value,
+//     userItemsIds: hideOwnedItemsModel.value ? userItemsIds.value : [],
+//     aggregationConfig: aggregationsConfig.value,
+//     sortingConfig: sortingConfig.value,
+//     sort: sortingModel.value,
+//     page: pageModel.value,
+//     perPage: perPageModel.value,
+//     query: searchModel.value,
+//     filter: {
+//       ...filterModel.value,
+//       ...(isCompare.value && { modId: compareList.value }),
+//     },
+//   })
+// );
+
+// const compareItemsResult = computed(() =>
+//   !isCompare.value
+//     ? null
+//     : getCompareItemsResult(searchResult.value.data.items, aggregationsConfig.value)
+// );
+
+// const buyItem = async (item: ItemFlat) => {
+//   await userStore.buyItem(item.id);
+
+//   notify(t('shop.item.buy.notify.success'));
+// };
+
+// const isUpgradableCategory = computed(() => canUpgrade(itemTypeModel.value));
+
+// const newItemCount = computed(
+//   () => searchResult.value.data.aggregations.new.buckets.find(b => b.key === '1')?.doc_count ?? 0
+// );
+
+//
+//
+//
+//
+//
+//
+//
+//
+const items2 = createItemIndex(items.value);
+const store = await defaultDocumentsStore.createDocumentsStore();
+const itemsDB = await create({
+  schema: {
+    // modId: 'string',
+    name: 'string',
+    type: 'string',
+  },
+
+  // components: {
+  //   // override partially the default documents store
+  //   documentsStore: {
+  //     ...store,
+  //     create(orama, mapper) {
+  //       console.log(orama);
+  //       console.log(mapper);
+
+  //       return {
+  //         idToInternalId: ()
+  //       };
+  //     },
+  //   },
+  // },
+});
+
+await insertMultiple(itemsDB, items.value, 100);
+
+const results = await search(itemsDB, {
+  term: 'can',
+  properties: ['name'],
+  // limit: 0,
+  facets: {
+    type: {
+      // size: 3,
+      // order: 'DESC',
     },
-  })
-);
-
-const compareItemsResult = computed(() =>
-  !isCompare.value
-    ? null
-    : getCompareItemsResult(searchResult.value.data.items, aggregationsConfig.value)
-);
-
-const buyItem = async (item: ItemFlat) => {
-  await userStore.buyItem(item.id);
-
-  notify(t('shop.item.buy.notify.success'));
-};
-
-const isUpgradableCategory = computed(() => canUpgrade(itemTypeModel.value));
-
-const newItemCount = computed(
-  () => searchResult.value.data.aggregations.new.buckets.find(b => b.key === '1')?.doc_count ?? 0
-);
+    // 'categories.secondary': {
+    //   size: 2,
+    //   order: 'DESC',
+    // },
+    // rating: {
+    //   ranges: [
+    //     { from: 0, to: 3 },
+    //     { from: 3, to: 7 },
+    //     { from: 7, to: 10 },
+    //   ],
+    // },
+    // isFavorite: {
+    //   true: true,
+    //   false: true,
+    // },
+  },
+});
+console.log('query', 'can');
+console.log('results', results);
 </script>
 
-<template>
-  <div class="relative space-y-2 px-6 pb-6 pt-6">
-    <!-- <div class="fixed top-4 right-10 z-20 rounded-lg bg-white p-4 shadow-lg">
-     <div>baseFilterModel: type: {{ itemTypeModel }} weaponClass: {{ weaponClassModel }}</div>
-      <div>filterModel: {{ filterModel }}</div>
-      <div>nameModel: {{ nameModel }}</div>
-      <div>sortingModel: {{ sortingModel }}</div>
-      <div>pageModel: {{ pageModel }}</div>
-      <div>searchResult.pagination.total {{ searchResult.pagination.total }}</div>
-    </div> -->
-
-    <div class="mb-2 flex items-center gap-6 overflow-x-auto pb-2">
-      <VDropdown :triggers="['click']" placement="bottom-end">
-        <MoreOptionsDropdownButton
-          :active="
-            hideOwnedItemsModel ||
-            Boolean('weaponUsage' in filterModel && filterModel['weaponUsage']!.length > 1) ||
-            Boolean('new' in filterModel && filterModel['new']!.length)
-          "
-        />
-
-        <template #popper="{ hide }">
-          <!-- TODO: to cmp -->
-          <DropdownItem>
-            <Tooltip
-              :title="$t('item.aggregations.new.title')"
-              :description="$t('item.aggregations.new.description', { days: itemIsNewDays })"
-            >
-              <OCheckbox
-                :nativeValue="1"
-                :modelValue="filterModel['new']"
-                :disabled="newItemCount === 0"
-                @update:modelValue="(val: number) => updateFilter('new', val)"
-                @change="hide"
-              >
-                {{ $t('item.aggregations.new.title') }}
-                ({{ newItemCount }})
-              </OCheckbox>
-            </Tooltip>
-          </DropdownItem>
-
-          <DropdownItem>
-            <OCheckbox v-model="hideOwnedItemsModel" @change="hide">
-              {{ $t('shop.hideOwnedItems.title') }}
-            </OCheckbox>
-          </DropdownItem>
-
-          <DropdownItem v-if="'weaponUsage' in filterModel">
-            <Tooltip
-              :title="$t('shop.nonPrimaryWeaponMode.tooltip.title')"
-              :description="$t('shop.nonPrimaryWeaponMode.tooltip.desc')"
-            >
-              <OCheckbox
-                :nativeValue="WeaponUsage.Secondary"
-                :modelValue="filterModel['weaponUsage']"
-                @update:modelValue="(val: string) => updateFilter('weaponUsage', val)"
-                @change="hide"
-              >
-                {{ $t('shop.nonPrimaryWeaponMode.title') }}
-              </OCheckbox>
-            </Tooltip>
-          </DropdownItem>
-        </template>
-      </VDropdown>
-
-      <div class="h-8 w-px select-none bg-border-200" />
-
-      <ShopItemTypeSelect
-        v-model:itemType="itemTypeModel"
-        v-model:weaponClass="weaponClassModel"
-        :itemTypeBuckets="aggregationByType.data.buckets"
-        :weaponClassBuckets="aggregationByClass.data.buckets"
-      />
-    </div>
-
-    <OTable
-      v-model:current-page="pageModel"
-      v-model:checked-rows="compareList"
-      :data="searchResult.data.items"
-      bordered
-      narrowed
-      hoverable
-      sortIcon="chevron-up"
-      sortIconSize="xs"
-      sticky-header
-      :detailed="isUpgradableCategory"
-      detailKey="id"
-      customRowKey="id"
-    >
-      <OTableColumn field="compare" :width="36">
-        <template #header>
-          <span class="inline-flex items-center">
-            <OCheckbox
-              v-tooltip="
-                compareList.length ? $t('shop.compare.removeAll') : $t('shop.compare.addAll')
-              "
-              :modelValue="compareList.length >= 1"
-              :nativeValue="true"
-              @update:modelValue="
-                () =>
-                  compareList.length
-                    ? removeAllFromCompareList()
-                    : addAllToCompareList(searchResult.data.items.map(item => item.modId))
-              "
-            />
-          </span>
-        </template>
-        <template #default="{ row: item }: { row: ItemFlat }">
-          <span class="inline-flex items-center">
-            <OCheckbox
-              v-tooltip="
-                compareList.includes(item.modId)
-                  ? $t('shop.compare.remove')
-                  : $t('shop.compare.add')
-              "
-              :modelValue="compareList.includes(item.modId)"
-              :nativeValue="true"
-              @update:modelValue="() => toggleToCompareList(item.modId)"
-            />
-          </span>
-        </template>
-      </OTableColumn>
-
-      <OTableColumn field="name">
-        <template #header>
-          <div class="max-w-[220px]">
-            <OInput
-              v-model="searchModel"
-              type="text"
-              :placeholder="$t('action.search')"
-              icon="search"
-              rounded
-              expanded
-              clearable
-              size="sm"
-              iconRightClickable
-              data-aq-search-shop-input
-            />
-          </div>
-        </template>
-
-        <template #default="{ row: item }: { row: ItemFlat }">
-          <ShopGridItemName :item="item" showTier />
-        </template>
-      </OTableColumn>
-
-      <OTableColumn
-        v-for="field in Object.keys(aggregationsConfigVisible) as Array<keyof ItemFlat>"
-        :field="field"
-        :width="aggregationsConfigVisible[field]?.width ?? 140"
-        :thAttrs="
-          () => ({
-            style: `max-width: ${aggregationsConfigVisible[field]?.width ?? 140}px`,
-          })
-        "
-      >
-        <template #header>
-          <div class="relative mr-2 flex items-center gap-1">
-            <ShopGridFilter
-              v-if="field in searchResult.data.aggregations"
-              :scopeAggregation="scopeAggregations[field]"
-              :aggregation="searchResult.data.aggregations[field]"
-              :aggregationConfig="aggregationsConfig[field]!"
-              :modelValue="filterModel[field]!"
-              @update:modelValue="val => updateFilter(field, val)"
-            />
-            <ShopGridSort
-              v-if="Object.keys(getSortingConfigByField(field)).length !== 0"
-              class="w-5"
-              v-model:modelValue="sortingModel"
-              :sortingConfig="getSortingConfigByField(field)"
-            />
-          </div>
-        </template>
-
-        <template #default="{ row: item }: { row: ItemFlat }">
-          <ItemParam
-            :item="item"
-            :field="field"
-            :bestValue="compareItemsResult !== null ? compareItemsResult[field] : undefined"
-            :isCompare="isCompare"
-          >
-            <template v-if="field === 'upkeep'" #default="{ rawBuckets }">
-              <Coin>
-                {{ $t('item.format.upkeep', { upkeep: $n(rawBuckets as number) }) }}
-              </Coin>
-            </template>
-            <template v-if="field === 'price'" #default="{ rawBuckets }">
-              <ShopGridItemBuyBtn
-                :price="rawBuckets"
-                :upkeep="item.upkeep"
-                :inInventory="userItemsIds.includes(item.id)"
-                :notEnoughGold="userStore.user!.gold < item.price"
-                @buy="buyItem(item)"
-              />
-            </template>
-          </ItemParam>
-        </template>
-      </OTableColumn>
-
-      <template #detail="{ row: item }: { row: ItemFlat }">
-        <ShopGridUpgradesTable :item="item" :cols="aggregationsConfigVisible" />
-      </template>
-
-      <template #empty>
-        <ResultNotFound />
-      </template>
-
-      <template #footer>
-        <div class="space-y-4 bg-base-100 py-4 pr-2 backdrop-blur-sm">
-          <div class="grid h-14 grid-cols-3 items-center gap-6">
-            <Pagination
-              v-model="pageModel"
-              :total="searchResult.pagination.total"
-              :perPage="searchResult.pagination.per_page"
-              order="left"
-              withInput
-            />
-
-            <div class="flex justify-center">
-              <OButton
-                v-if="compareList.length >= 2"
-                variant="primary"
-                size="lg"
-                outlined
-                :iconRight="isCompare ? 'close' : null"
-                data-aq-shop-handler="toggle-compare"
-                :label="$t('shop.compare.title')"
-                @click="toggleCompare"
-              />
-            </div>
-
-            <div class="flex items-center justify-end gap-4">
-              <div class="text-content-400">{{ $t('shop.pagination.perPage') }}</div>
-              <OTabs v-model="perPageModel" size="xl" type="bordered-rounded" contentClass="hidden">
-                <OTabItem v-for="pp in perPageConfig" :label="String(pp)" :value="pp" />
-              </OTabs>
-            </div>
-          </div>
-        </div>
-      </template>
-    </OTable>
-  </div>
-</template>
+<template>ddd</template>
