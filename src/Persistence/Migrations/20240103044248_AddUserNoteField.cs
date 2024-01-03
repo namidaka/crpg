65d5d9b034381fe82013535c00aeb6cbe68758c0
Valeriy Sinevich <zohan33333@gmail.com>
2024-01-03 04:43:12 +0000

    user note migration

---
+++ src/Persistence/Migrations/20240103044248_AddUserNoteField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Crpg.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserNoteField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "note",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "note",
+                table: "users");
+        }
+    }
+}
