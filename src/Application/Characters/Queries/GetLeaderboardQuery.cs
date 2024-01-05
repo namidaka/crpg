1d7873c24aed73500c695b01df3d319d1fce29d7
Valeriy Sinevich <zohan33333@gmail.com>
2024-01-05 16:00:51 +0000

    inMemory cache for leaderboard query

--- src/Application/Characters/Queries/GetLeaderboardQuery.cs
+++ src/Application/Characters/Queries/GetLeaderboardQuery.cs
@@ -7,54 +7,72 @@ using Crpg.Application.Common.Results;
 using Crpg.Domain.Entities;
 using Crpg.Domain.Entities.Characters;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Crpg.Application.Characters.Queries;
 
 public record GetLeaderboardQuery : IMediatorRequest<IList<CharacterPublicViewModel>>
 {
     public Region? Region { get; set; }
-
     public CharacterClass? CharacterClass { get; set; }
 
     internal class Handler : IMediatorRequestHandler<GetLeaderboardQuery, IList<CharacterPublicViewModel>>
     {
         private readonly ICrpgDbContext _db;
         private readonly IMapper _mapper;
+        private readonly IMemoryCache _cache;
 
-        public Handler(ICrpgDbContext db, IMapper mapper)
+        public Handler(ICrpgDbContext db, IMapper mapper, IMemoryCache cache)
         {
             _db = db;
             _mapper = mapper;
+            _cache = cache;
         }
 
         public async Task<Result<IList<CharacterPublicViewModel>>> Handle(GetLeaderboardQuery req, CancellationToken cancellationToken)
         {
-            var topRatedCharactersByRegion = await _db.Characters
+            string cacheKey = GetCacheKey(req);
+
+            if (_cache.TryGetValue(cacheKey, out IList<CharacterPublicViewModel>? results) == false)
+            {
+                var topRatedCharactersByRegion = await _db.Characters
                 .OrderByDescending(c => c.Rating.CompetitiveValue)
                 .Where(c => (req.Region == null || req.Region == c.User!.Region)
                             && (req.CharacterClass == null || req.CharacterClass == c.Class))
                 .ProjectTo<CharacterPublicViewModel>(_mapper.ConfigurationProvider)
                 .ToArrayAsync(cancellationToken);
 
-            var results = new List<CharacterPublicViewModel>();
+                IList<CharacterPublicViewModel> data = new List<CharacterPublicViewModel>();
 
-            // DistinctBy don't work https://github.com/dotnet/efcore/issues/27470, let's do it manually.
-            foreach (var result in topRatedCharactersByRegion)
-            {
-                if (results.Count >= 50)
+                // DistinctBy don't work https://github.com/dotnet/efcore/issues/27470, let's do it manually.
+                foreach (var result in topRatedCharactersByRegion)
                 {
-                    break;
+                    if (data.Count >= 50)
+                    {
+                        break;
+                    }
+
+                    if (data.Count(c => c.User.Id == result.User.Id) > 0)
+                    {
+                        continue;
+                    }
+
+                    data.Add(result);
                 }
 
-                if (results.Count(c => c.User.Id == result.User.Id) > 0)
-                {
-                    continue;
-                }
+                var cacheOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(1));
+                _cache.Set(cacheKey, data, cacheOptions);
 
-                results.Add(result);
+                return new(data);
             }
 
             return new(results);
+        }
+
+        private string GetCacheKey(GetLeaderboardQuery req)
+        {
+            return $"leaderboard::{req.Region}::{req.CharacterClass}";
         }
     }
 }
