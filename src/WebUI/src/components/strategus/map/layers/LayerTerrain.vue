f183bc8986c3a2a559553f2c46947e38aee2a148
Valeriy Sinevich <zohan33333@gmail.com>
2024-01-08 18:31:38 +0000

    bring to back terrain layer

--- src/WebUI/src/components/strategus/map/layers/LayerTerrain.vue
+++ src/WebUI/src/components/strategus/map/layers/LayerTerrain.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import L from 'leaflet';
+import L, { GeoJSON } from 'leaflet';
 import { LGeoJson } from '@vue-leaflet/vue-leaflet';
 import { type TerrainFeature, type TerrainFeatureCollection } from '@/models/strategus/terrain';
-import { TerrainColorByType } from '@/services/strategus-service/terrain';
+import { terrainColorByType } from '@/services/strategus-service/terrain';
+import { t } from '@/services/translate-service';
 
 const { data } = defineProps<{
   data: TerrainFeatureCollection;
@@ -12,8 +13,10 @@ const emit = defineEmits<{
   update: [e: L.PM.UpdateEventHandler];
 }>();
 
+const geoJSON = ref<typeof LGeoJson | null>(null);
+
 const terrainGeoJSONStyle = (feature: TerrainFeature) => ({
-  color: TerrainColorByType[feature.properties.type],
+  color: terrainColorByType[feature.properties.type],
 });
 
 const onEachFeatureFunction = (feature: TerrainFeature, layer: L.Polygon) => {
@@ -24,14 +27,37 @@ const onEachFeatureFunction = (feature: TerrainFeature, layer: L.Polygon) => {
 
   // @ts-ignore TODO:
   layer.on('pm:update', e => emit('update', e));
+
+  layer.bindTooltip(
+    `
+    <div>${t(`strategus.terrainType.${feature.properties.type}`)} - TODO: (ex. penalty)</div>`,
+    {
+      permanent: false,
+      sticky: true,
+      direction: 'bottom',
+      offset: [0, 16],
+    }
+  );
 };
 
 const terrainGeoJSONOptions = {
   onEachFeature: onEachFeatureFunction,
 };
+
+const onReady = () => {
+  if (geoJSON.value === null) return;
+
+  (geoJSON.value.leafletObject as GeoJSON).bringToBack();
+};
 </script>
 
 <template>
   <!-- TODO: ts -->
-  <LGeoJson :geojson="data" :optionsStyle="terrainGeoJSONStyle" :options="terrainGeoJSONOptions" />
+  <LGeoJson
+    ref="geoJSON"
+    :geojson="data"
+    :optionsStyle="terrainGeoJSONStyle"
+    :options="terrainGeoJSONOptions"
+    @ready="onReady"
+  />
 </template>
