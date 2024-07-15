006e2435e9427a069d8a8e28b91f2a5fc7e0f01f
Valeriy Sinevich <zohan33333@gmail.com>
2024-07-15 22:14:57 +0000

    ui d1

--- src/WebUI/src/components/user/UserHeaderToolbar.vue
+++ src/WebUI/src/components/user/UserHeaderToolbar.vue
@@ -35,12 +35,75 @@ const animatedUserGold = useTransition(toRef(() => userStore.user!.gold));
 
     <VDropdown placement="bottom-end">
       <template #default="{ shown }">
-        <OButton
-          :variant="shown ? 'transparent-active' : 'transparent'"
-          size="sm"
-          rounded
-          iconLeft="dots"
-        />
+        <OButton :variant="shown ? 'transparent-active' : 'transparent'" size="sm" rounded>
+          <FontAwesomeLayers full-width class="fa-2x">
+            <FontAwesomeIcon :icon="['crpg', 'carillon']" />
+            <!-- v-if="active" -->
+            <FontAwesomeLayersText
+              counter
+              value="●"
+              position="top-right"
+              :style="{ '--fa-counter-background-color': 'rgba(83, 188, 150, 1)' }"
+            />
+          </FontAwesomeLayers>
+        </OButton>
+      </template>
+
+      <template #popper="{ hide }">
+        <!-- TODO: merge with activity log card -->
+        <DropdownItem>
+          <div class="flex max-w-xl flex-col space-y-2">
+            <div class="flex items-center gap-2">
+              <!-- TODO: create System User cmp -->
+              <div class="flex items-center gap-1.5 text-content-100">
+                <SvgSpriteImg name="logo" viewBox="0 0 162 124" class="w-6" />
+                System
+              </div>
+
+              <div class="text-2xs text-content-300">
+                {{ $d(new Date('2024-07-15T20:26:06.2775662Z'), 'short') }}
+              </div>
+              <Tag variant="primary" :label="'CharacterEarned'" />
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur tempore at,
+              ullam unde nobis vitae atque mollitia dolore laboriosam cum ipsam incidunt saepe odit
+              aperiam obcaecati, maxime eum. Aliquid inventore cum culpa!
+            </div>
+          </div>
+        </DropdownItem>
+
+        <Divider />
+
+        <DropdownItem>
+          <div class="flex max-w-xl flex-col space-y-2">
+            <div class="flex items-center gap-2">
+              <div class="flex items-center gap-1.5 text-content-100">
+                <SvgSpriteImg name="logo" viewBox="0 0 162 124" class="w-6" />
+                System
+              </div>
+              <div class="text-2xs text-content-300">
+                {{ $d(new Date('2024-07-15T20:26:06.2775662Z'), 'long') }}
+              </div>
+              <Tag variant="primary" :label="'CharacterEarned'" />
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur tempore at,
+              ullam unde nobis vitae atque mollitia dolore laboriosam cum ipsam incidunt saepe odit
+              aperiam obcaecati, maxime eum. Aliquid inventore cum culpa!
+            </div>
+          </div>
+        </DropdownItem>
+      </template>
+    </VDropdown>
+
+    <VDropdown placement="bottom-end">
+      <template #default="{ shown }">
+        <OButton :variant="shown ? 'transparent-active' : 'transparent'" size="sm" rounded>
+          <FontAwesomeLayers full-width class="fa-2x">
+            <FontAwesomeIcon :icon="['crpg', 'dots']" />
+          </FontAwesomeLayers>
+        </OButton>
       </template>
 
       <template #popper="{ hide }">
