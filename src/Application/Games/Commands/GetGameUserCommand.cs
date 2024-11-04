3157d6354b27787eed56844dbe9f47553914af15
Harry Gardner <harrygardner123@gmail.com>
2024-11-04 14:48:47 +0000

    update instance parsing

--- src/Application/Games/Commands/GetGameUserCommand.cs
+++ src/Application/Games/Commands/GetGameUserCommand.cs
@@ -14,6 +14,7 @@ using Crpg.Sdk.Abstractions;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 using LoggerFactory = Crpg.Logging.LoggerFactory;
 
 namespace Crpg.Application.Games.Commands;
@@ -216,12 +217,7 @@ public record GetGameUserCommand : IMediatorRequest<GameUserViewModel>
                     .Include(ei => ei.UserItem)
                     .LoadAsync(cancellationToken);
 
-                if (!Enum.TryParse(req.Instance, true, out GameModeAlias instanceAlias))
-                {
-                    instanceAlias = GameModeAlias.Z; // Default value if parsing fails.
-                }
-
-                GameMode currentGameMode = _gameModeService.GameModeByInstanceAlias(instanceAlias);
+                GameMode currentGameMode = _gameModeService.GameModeByInstanceAlias(Enum.TryParse(req.Instance[^1..], ignoreCase: true, out GameModeAlias instanceAlias) ? instanceAlias : GameModeAlias.Z);
 
                 var statistics = await _db.Entry(user.ActiveCharacter)
                     .Collection(c => c.Statistics)
